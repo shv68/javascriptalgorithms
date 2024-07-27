762c1fdf7e1b7178ce8653c2985da4eadcd93935
Harsha Vardan <hk.harshapshv@gmail.com>
2024-07-27 12:38:51 +0530

    step-5-pyramid

--- pyramidgenerator/script.js
+++ pyramidgenerator/script.js
@@ -5,3 +5,11 @@ const rows = [];
 for (let i = 0; i < count; i = i + 1) {
   rows.push(character);
 }
+
+let result = "";
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
