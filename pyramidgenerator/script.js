c0dfc3b38a3beb85e3b3eb0bf0ac8e09db6c4ad7
Harsha Vardan <hk.harshapshv@gmail.com>
2024-07-27 14:15:37 +0530

    step-8-pyramid

--- pyramidgenerator/script.js
+++ pyramidgenerator/script.js
@@ -2,6 +2,10 @@ const character = "#";
 const count = 8;
 const rows = [];
 
+function padRow(rowNumber, rowCount) {
+  return character.repeat(rowNumber);
+}
+
 for (let i = 0; i < count; i = i + 1) {
   rows.push(character.repeat(i + 1));
 }
