ae064c65a4d68834b23968dcf9fc59b5a7f97c16
Harsha Vardan <hk.harshapshv@gmail.com>
2024-07-29 15:14:36 +0530

    rpg-step-88

--- roleplay/script.js
+++ roleplay/script.js
@@ -112,7 +112,7 @@ function buyWeapon() {
     gold -= 30;
     currentWeaponIndex++;
     goldText.innerText = gold;
-    let newWeapon = weapons[currentWeaponIndex];
+    let newWeapon = weapons[currentWeaponIndex].name;
     text.innerText = "You now have a new weapon.";
   }
 }
