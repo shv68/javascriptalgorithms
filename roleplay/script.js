8ac4607a6ed4954e125839aa4a17bc701ac92f8a
Harsha Vardan <hk.harshapshv@gmail.com>
2024-07-29 21:33:01 +0530

    rpg-step-148

--- roleplay/script.js
+++ roleplay/script.js
@@ -217,6 +217,8 @@ function attack() {
   if (isMonsterHit()) {
     monsterHealth -=
       weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  } else {
+    text.innerText += " You miss.";
   }
   if (health <= 0) {
     lose();
