fd7c15934ce636e9cbbee220f26f17b2b4618fad
Harsha Vardan <hk.harshapshv@gmail.com>
2024-07-29 12:04:29 +0530

    rpg-step-36

--- roleplay/script.js
+++ roleplay/script.js
@@ -23,6 +23,8 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 //initialize buttons
 
 button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
 
 function goStore() {
   console.log("Going to store.");
