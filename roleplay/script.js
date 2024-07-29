838ff28584f845c9f8467211b6e4a5276d267f3f
Harsha Vardan <hk.harshapshv@gmail.com>
2024-07-29 16:39:19 +0530

    rpg-step-98

--- roleplay/script.js
+++ roleplay/script.js
@@ -120,6 +120,8 @@ function buyWeapon() {
     } else {
       text.innerText = "You do not have enough gold to buy a weapon.";
     }
+  } else {
+    text.innerText = "You already have the most powerful weapon!";
   }
 }
 
