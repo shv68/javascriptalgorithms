ee1b44e4471b0f0bd43ae286ecd51dfa2e0f4159
Harsha Vardan <hk.harshapshv@gmail.com>
2024-07-29 21:52:13 +0530

    rpg-step-161

--- roleplay/script.js
+++ roleplay/script.js
@@ -289,7 +289,9 @@ function easterEgg() {
 }
 function pick(guess) {
   const numbers = [];
-  while (numbers.length < 10) {}
+  while (numbers.length < 10) {
+    numbers.push(Math.floor(Math.random() * 11));
+  }
 }
 
 function pickTwo() {
