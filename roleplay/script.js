556c35cac92ff698c74b68ada7403866221415da
Harsha Vardan <hk.harshapshv@gmail.com>
2024-07-29 20:32:39 +0530

    rpg-step-119

--- roleplay/script.js
+++ roleplay/script.js
@@ -185,5 +185,7 @@ function goFight() {
 
 function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
+  text.innerText +=
+    " You attack it with your " + weapons[currentWeaponIndex].name + ".";
 }
 function dodge() {}
