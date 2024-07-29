7c85e169c204e027e74526221e391803c03d3d32
Harsha Vardan <hk.harshapshv@gmail.com>
2024-07-29 20:49:59 +0530

    rpg-step-130

--- roleplay/script.js
+++ roleplay/script.js
@@ -205,6 +205,8 @@ function dodge() {
 function defeatMonster() {
   gold += Math.floor(monsters[fighting].level * 6.7);
   xp += monsters[fighting].level;
+  goldText.innerText = gold;
+  xpText.innerText = xp;
 }
 
 function lose() {}
