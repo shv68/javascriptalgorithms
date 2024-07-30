aa910155cc8c81605bc71964e4a8cf1e7c5c89db
Harsha Vardan <hk.harshapshv@gmail.com>
2024-07-30 22:42:42 +0530

    rps-step-3

--- rockpaperscissors/script.js
+++ rockpaperscissors/script.js
@@ -4,17 +4,29 @@ function getRandomComputerResult() {
   return options[randomIndex];
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
 function hasPlayerWonTheRound(player, computer) {
-  if (player == "Rock" && computer == "Scissors") {
-    return true;
-  } else if (player == "Scissors" && computer == "Paper") {
-    return true;
-  } else if (player == "Paper" && computer == "Rock") {
-    return true;
+  return (
+    (player === "Rock" && computer === "Scissors") ||
+    (player === "Scissors" && computer === "Paper") ||
+    (player === "Paper" && computer === "Rock")
+  );
+}
+
+function getRoundResults(userOption) {
+  const computerResult = getRandomComputerResult();
+  if (userOption === computerResult) {
+    return "Its a tie! Both chose " + userOption;
+  } else if (hasPlayerWonTheRound) {
+    playerScore++;
+    return "Player wins! " + userOption + " beats " + computerResult;
   } else {
-    return false;
+    computerScore++;
+    return "Computer Wins! " + computerResult + " beats " + userOption;
   }
 }
 
-console.log(hasPlayerWonTheRound("Rock", "Scissors"));
-console.log(hasPlayerWonTheRound("Scissors", "Rock"));
+console.log(getRoundResults("Rock"));
+console.log("Player Score: ", playerScore, "Computer Score: ", computerScore);
