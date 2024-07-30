9790f71637da2a6d9990b20389d870523da23e3d
Harsha Vardan <hk.harshapshv@gmail.com>
2024-07-30 22:01:08 +0530

    ccounter-step-96

--- caloriecounter/script.js
+++ caloriecounter/script.js
@@ -107,3 +107,5 @@ function clearForm() {
 
 addEntryButton.addEventListener("click", addEntry);
 calorieCounter.addEventListener("submit", calculateCalories);
+
+clearButton.addEventListener("click", clearForm);
