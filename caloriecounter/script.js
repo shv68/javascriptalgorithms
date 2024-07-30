50f71d850df1d66f78e7d7cb3c2e2ec4be5316b7
Harsha Vardan <hk.harshapshv@gmail.com>
2024-07-30 20:58:24 +0530

    ccounter-step-69

--- caloriecounter/script.js
+++ caloriecounter/script.js
@@ -37,6 +37,9 @@ function calculateCalories(e) {
   const breakfastNumberInputs = document.querySelectorAll(
     "#breakfast input[type=number]"
   );
+  const lunchNumberInputs = document.querySelectorAll(
+    "#lunch input[type=number]"
+  );
 }
 
 function getCaloriesFromInputs(list) {
