e44822cd6fb33af8884150d8fb543b1af7f21660
Harsha Vardan <hk.harshapshv@gmail.com>
2024-07-30 12:03:28 +0530

    bgc-step-5

--- bgchanger/script.js
+++ bgchanger/script.js
@@ -16,5 +16,8 @@ function getRandomIndex() {
 }
 
 const body = document.querySelector("body");
+const bgHexCodeSpanElement = document.querySelector("#bg-hex-code");
+
+console.log(bgHexCodeSpanElement);
 
 getRandomIndex();
