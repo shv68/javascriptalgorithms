98dc17b87f07bd8ce5b75c1a9f5ff08aa3245658
Harsha Vardan <hk.harshapshv@gmail.com>
2024-07-31 12:49:36 +0530

    musicplayer-step-16

--- musicplayer/script.js
+++ musicplayer/script.js
@@ -85,19 +85,3 @@ let userData = {
   currentSong: null,
   songCurrentTime: 0,
 };
-
-const printGreeting = () => {
-  console.log("Hello there!");
-};
-
-printGreeting();
-
-const printMessage = (org) => {
-  console.log(`${org} is awesome!`);
-};
-
-printMessage("freeCodeCamp");
-
-let addTwoNumbers = (num1, num2) => num1 + num2;
-
-console.log(addTwoNumbers(3, 4));
