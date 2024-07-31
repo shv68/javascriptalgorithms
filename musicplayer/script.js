961bc2bd7353f2b609b39fa6583de2ced18852fa
Harsha Vardan <hk.harshapshv@gmail.com>
2024-07-31 21:25:22 +0530

    musicplayer-step-42

--- musicplayer/script.js
+++ musicplayer/script.js
@@ -123,6 +123,8 @@ const renderSongs = (array) => {
   playlistSongs.innerHTML = songsHTML;
 };
 
+playButton.addEventListener("click", () => {});
+
 const sortSongs = () => {
   userData?.songs.sort((a, b) => {
     if (a.title < b.title) {
