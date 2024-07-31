20f79b47915314659d83d714abb47d2aad0c1c69
Harsha Vardan <hk.harshapshv@gmail.com>
2024-07-31 22:49:23 +0530

    musicplayer-step-101

--- musicplayer/script.js
+++ musicplayer/script.js
@@ -222,6 +222,11 @@ audio.addEventListener("ended", () => {
   } else {
     userData.currentSong = null;
     userData.songCurrentTime = 0;
+    pauseSong();
+    setPlayerDisplay();
+    highlightCurrentSong();
+    setPlayButtonAccessibleText();
+  }
 });
 
 const sortSongs = () => {
