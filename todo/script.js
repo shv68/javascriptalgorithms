7e2b97d026bd405d06ba8658bc116f4a731a1f13
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-01 21:28:24 +0530

    todo-step-42

--- todo/script.js
+++ todo/script.js
@@ -37,13 +37,15 @@ const updateTaskContainer = () => {
         <div class="task" id="${id}">
           <p><strong>Title:</strong> ${title}</p>
           <p><strong>Date:</strong> ${date}</p>
-                    <p><strong>Description:</strong> ${description}</p>
+          <p><strong>Description:</strong> ${description}</p>
           <button onclick="editTask(this)" type="button" class="btn">Edit</button>
           <button onclick="deleteTask(this)" type="button" class="btn">Delete</button>
         </div>
       `;
   });
 };
+
+const deleteTask = (buttonEl) => {};
 
 const reset = () => {
   titleInput.value = "";
