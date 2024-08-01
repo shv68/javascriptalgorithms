bef68091ca7e7c8117f0bd1c608c9c620008cdac
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-01 14:47:37 +0530

    fbtc-step-31

--- fbtc/script.js
+++ fbtc/script.js
@@ -186,6 +186,7 @@ const setPlayerCards = (arr = players) => {
       `<div class="player-card">
       <h2>${isCaptain ? "(Captain)" : ""} ${name}</h2>
       <p>Position: ${position}</p>
+      <p>Number: ${number}</p>
       </div>`;
     }
   );
