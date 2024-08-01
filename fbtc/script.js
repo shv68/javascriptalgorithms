fcdf073a4d86c62e5e9a43ffbb4d7f6301ad514e
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-01 14:28:24 +0530

    fbtc-step-17

--- fbtc/script.js
+++ fbtc/script.js
@@ -172,5 +172,4 @@ const myFavoriteFootballTeam = {
 };
 
 Object.freeze(myFavoriteFootballTeam);
-const sport = myFavoriteFootballTeam.sport;
-const team = myFavoriteFootballTeam.team;
+const { sport, team } = myFavoriteFootballTeam;
