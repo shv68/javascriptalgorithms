f765b8199671b2400283aa082fcdfb0a4e089605
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-01 13:19:03 +0530

    fbtc-step-12

--- fbtc/script.js
+++ fbtc/script.js
@@ -13,5 +13,13 @@ const myFavoriteFootballTeam = {
     coachName: "Carlos Bilardo",
     matches: 7,
   },
-  players: [],
+  players: [
+    {
+      name: "Sergio Almirón",
+      position: "forward",
+      number: 1,
+      isCaptain: false,
+      nickname: null,
+    },
+  ],
 };
