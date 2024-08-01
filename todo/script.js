e397ccdc882cfc28d3333201f7d661fe662ecfa5
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-01 15:18:21 +0530

    todo-step-12

--- todo/script.js
+++ todo/script.js
@@ -32,4 +32,7 @@ taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+
+  const taskObj = {};
+  console.log(taskObj);
 });
