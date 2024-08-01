030f2cf1f1249b6ceec9b01770b54688457be65f
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-01 14:52:52 +0530

    fbtc-step-36

--- fbtc/script.js
+++ fbtc/script.js
@@ -193,4 +193,4 @@ const setPlayerCards = (arr = players) => {
     .join("");
 };
 
-playersDropdownList.addEventListener("change", () => {});
+playersDropdownList.addEventListener("change", (e) => {});
