6058b274fb65b78c4fd89556db08aa186998cfef
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-01 12:46:31 +0530

    df-step-22

--- dateformatter/script.js
+++ dateformatter/script.js
@@ -11,8 +11,6 @@ const minutes = date.getMinutes();
 const formattedDate = `${day}-${month}-${year}`;
 currentDateParagraph.textContent = formattedDate;
 
-const exampleSentence = "selur pmaCedoCeerf".split("").reverse().join("");
-
 dateOptionsSelectElement.addEventListener("change", () => {
   switch (dateOptionsSelectElement.value) {
     case "yyyy-mm-dd":
