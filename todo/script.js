d8904f97a03024908a922d19d1c7bdc3246c32ad
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-01 15:29:51 +0530

    todo-step-23

--- todo/script.js
+++ todo/script.js
@@ -42,5 +42,8 @@ taskForm.addEventListener("submit", (e) => {
   if (dataArrIndex === -1) {
     taskData.unshift(taskObj);
   }
-  taskData.forEach({ id, title, date, description });
+  taskData.forEach(({id, title, date, description}) => {
+    (tasksContainer.innerHTML += `
+        
+    `)
 });
