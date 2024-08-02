ff6a3e80d719ea6a9ff58c42bf8d2b759cfff121
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-02 22:24:26 +0530

    filter-step-10

--- spamfilter/script.js
+++ spamfilter/script.js
@@ -2,7 +2,7 @@ const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
-const helpRegex = /please help/i;
+const helpRegex = /please help|assist me/i;
 
 const isSpam = (msg) => helpRegex.test(msg);
 
