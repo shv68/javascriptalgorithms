a2411134b0e853f89672227fc19e2a3f65ecba67
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-02 21:29:50 +0530

    binary-step-84

--- binaryconverter/script.js
+++ binaryconverter/script.js
@@ -1,6 +1,7 @@
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
+const animationData = [];
 
 const decimalToBinary = (input) => {
   if (input === 0 || input === 1) {
