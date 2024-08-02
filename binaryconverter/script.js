c1438280f10eb7ff3179c571fc562d7aed1c958a
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-02 21:13:07 +0530

    binary-step-66

--- binaryconverter/script.js
+++ binaryconverter/script.js
@@ -2,29 +2,18 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
-const countDownAndUp = (number) => {
-  console.log(number);
-
-  if (number === 0) {
-    console.log("Reached base case");
-    return;
-  } else {
-    countDownAndUp(number - 1);
-    console.log(number);
-  }
-};
-
-countDownAndUp(3);
-
 const decimalToBinary = (input) => {
   let binary = "";
+
   if (input === 0) {
     binary = "0";
   }
+
   while (input > 0) {
     binary = (input % 2) + binary;
     input = Math.floor(input / 2);
   }
+
   result.innerText = binary;
 };
 
@@ -37,6 +26,7 @@ const checkUserInput = () => {
     alert("Please provide a decimal number greater than or equal to 0");
     return;
   }
+
   decimalToBinary(parseInt(numberInput.value));
   numberInput.value = "";
 };
