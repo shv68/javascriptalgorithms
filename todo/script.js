b8d1c049afc062f5194ec834c5e562f0bae4e3b5
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-02 12:57:42 +0530

    todo-step-46

--- todo/script.js
+++ todo/script.js
@@ -53,7 +53,11 @@ const deleteTask = (buttonEl) => {
   taskData.splice(dataArrIndex, 1);
 };
 
-const editTask = (buttonEl) => {};
+const editTask = (buttonEl) => {
+  const dataArrIndex = taskData.findIndex(
+    (item) => item.id === buttonEl.parentElement.id
+  );
+};
 
 const reset = () => {
   titleInput.value = "";
