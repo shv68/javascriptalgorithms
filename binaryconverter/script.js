781ff16e1677d98d47223812c233ab49dac07fbc
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-02 21:25:13 +0530

    binary-step-77

--- binaryconverter/script.js
+++ binaryconverter/script.js
@@ -22,6 +22,8 @@ const checkUserInput = () => {
     return;
   }
   if (parseInt(numberInput.value) === 5) {
+    showAnimation();
+    return;
   }
   result.textContent = decimalToBinary(parseInt(numberInput.value));
   numberInput.value = "";
