ca9ac03a1eab83780fe68d10930e00c473e1f967
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-02 21:40:28 +0530

    binary-step-95

--- binaryconverter/script.js
+++ binaryconverter/script.js
@@ -33,7 +33,7 @@ const showAnimation = () => {
   result.innerText = "Call Stack Animation";
   animationData.forEach((obj) => {
     setTimeout(() => {
-      animationContainer.innerHTML += `<p id=""></p>`;
+      animationContainer.innerHTML += `<p id="${obj.inputVal}"></p>`;
     }, obj.addElDelay);
   });
 };
