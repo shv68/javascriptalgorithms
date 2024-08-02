15539668a9214d013652a59ef36076ea42ed2860
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-02 15:27:20 +0530

    binary-step-30

--- binaryconverter/script.js
+++ binaryconverter/script.js
@@ -10,6 +10,8 @@ const decimalToBinary = (input) => {
     const quotient = Math.floor(input / 2);
     const remainder = input % 2;
     inputs.push(input);
+    quotients.push(quotient);
+    remainders.push(remainder);
     input = quotient;
   }
 };
