461f2a8d0b25f94893d408d3ebbffded14d119a7
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-02 21:52:15 +0530

    binary-step-105

--- binaryconverter/script.js
+++ binaryconverter/script.js
@@ -50,7 +50,7 @@ const showAnimation = () => {
     }, obj.addElDelay);
 
     setTimeout(() => {
-      document.getElementById(obj.inputVal);
+      document.getElementById(obj.inputVal).textContent = obj.msg;
     }, obj.showMsgDelay);
   });
 };
