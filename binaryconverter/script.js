29569f03c10cd86482a071a9f54b7bf0d253bcd2
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-02 15:58:56 +0530

    binary-step-52

--- binaryconverter/script.js
+++ binaryconverter/script.js
@@ -1,7 +1,4 @@
-const callStack = [
-  'a(): returns "freeCodeCamp " + b()',
-  "b(): returns 'is ' + 'awesome!'",
-];
+const callStack = ["b(): returns 'is ' + 'awesome!'"];
 const a = () => {
   return "freeCodeCamp " + b();
 };
