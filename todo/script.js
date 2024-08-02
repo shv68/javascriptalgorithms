4dce1eccf84152437856103da0d6116d0824d711
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-02 13:07:34 +0530

    todo-step-51

--- todo/script.js
+++ todo/script.js
@@ -24,6 +24,8 @@ const addOrUpdateTask = () => {
 
   if (dataArrIndex === -1) {
     taskData.unshift(taskObj);
+  } else {
+    taskData[dataArrIndex] = taskObj;
   }
   updateTaskContainer();
   reset();
