037556f637a5e7a55453aedb4bfb497a1937dcc2
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-03 15:09:49 +0530

    calc-step-23

--- calc/script.js
+++ calc/script.js
@@ -5,15 +5,19 @@ const testArr1 = [1, 2, 3, 4, 5];
 const testArr2 = [1, 2, 3, 4, 5, 6];
 const isEven = testArr2.length % 2 === 0;
 console.log(isEven);
+const oddListMedian = testArr1[Math.floor(testArr1.length / 2)];
+console.log(oddListMedian);
 
 const getMedian = (array) => {
   const sorted = array.sort((a, b) => a - b);
 };
 
 const calculate = () => {
-  let value = document.querySelector("#numbers").value;
+  const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
   const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
+
   const mean = getMean(numbers);
-  document.querySelector("#mean").textContent = mean.value();
+
+  document.querySelector("#mean").textContent = mean;
 };
