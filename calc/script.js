c993245980d8d4e87f354d393e4c911eb2cdad52
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-03 20:30:58 +0530

    calc-step-42

--- calc/script.js
+++ calc/script.js
@@ -32,7 +32,9 @@ const calculate = () => {
 
   const mean = getMean(numbers);
   const median = getMedian(numbers);
+  const mode = getMode(numbers);
 
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
+  document.querySelector("#mode").textContent = mode;
 };
