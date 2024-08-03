c239853fa59b6c84befd9df55f9822ecf5e36216
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-03 20:47:12 +0530

    calc-step-54

--- calc/script.js
+++ calc/script.js
@@ -42,6 +42,7 @@ const getVariance = (array) => {
 
 const getStandardDeviation = (array) => {
   const variance = getVariance(array);
+  const standardDeviation = Math.pow(variance, 1 / 2);
 };
 
 const calculate = () => {
