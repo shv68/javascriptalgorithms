896c2656464c6c59577c377644822174140af1e1
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-03 15:20:41 +0530

    calc-step-26

--- calc/script.js
+++ calc/script.js
@@ -3,6 +3,11 @@ const getMean = (array) =>
 
 const getMedian = (array) => {
   const sorted = array.sort((a, b) => a - b);
+  const median =
+    array.length % 2 === 0
+      ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]])
+      : sorted[Math.floor(array.length / 2)];
+  return median;
 };
 
 const calculate = () => {
