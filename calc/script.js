115a3f6032ef27a6a79fdcc5eeff83c69223bf73
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-03 14:56:08 +0530

    calc-step-16

--- calc/script.js
+++ calc/script.js
@@ -5,4 +5,5 @@ const calculate = () => {
   let value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
   const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
+  const mean = getMean(numbers);
 };
