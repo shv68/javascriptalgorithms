ffa95318f88fbe831bff8a08398b5fa0f6b4dda3
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-04 16:01:29 +0530

    cart-step-34

--- cart/script.js
+++ cart/script.js
@@ -134,5 +134,8 @@ class ShoppingCart {
       `);
   }
 }
+
 const cart = new ShoppingCart();
 const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
+
+[...addToCartBtns].forEach();
