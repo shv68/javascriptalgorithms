a6d97869b264808533be6071e5f87e5ba2970964
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-04 16:33:55 +0530

    game-step-16

--- game/script.js
+++ game/script.js
@@ -4,7 +4,6 @@ const startScreen = document.querySelector(".start-screen");
 const checkpointScreen = document.querySelector(".checkpoint-screen");
 const checkpointMessage = document.querySelector(".checkpoint-screen > p");
 const ctx = canvas.getContext("2d");
-
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 const gravity = 0.5;
@@ -13,11 +12,16 @@ let isCheckpointCollisionDetectionActive = true;
 const proportionalSize = (size) => {
   return innerHeight < 500 ? Math.ceil((size / 500) * innerHeight) : size;
 };
+
 class Player {
   constructor() {
     this.position = {
       x: proportionalSize(10),
       y: proportionalSize(400),
+    };
+    this.velocity = {
+      x: 0,
+      y: 0,
     };
   }
 }
