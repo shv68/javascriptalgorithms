d8e6871b1fa1ce9eb179274e14ead14cc31cc8a9
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-04 21:12:48 +0530

    dice-step-1

---
+++ dicegame/script.js
@@ -0,0 +1,18 @@
+const listOfAllDice = document.querySelectorAll(".die");
+const scoreInputs = document.querySelectorAll("#score-options input");
+const scoreSpans = document.querySelectorAll("#score-options span");
+const currentRound = document.getElementById("current-round");
+const currentRoundRolls = document.getElementById("current-round-rolls");
+const totalScore = document.getElementById("total-score");
+const scoreHistory = document.getElementById("score-history");
+const rollDiceBtn = document.getElementById("roll-dice-btn");
+const keepScoreBtn = document.getElementById("keep-score-btn");
+const rulesContainer = document.querySelector(".rules-container");
+const rulesBtn = document.getElementById("rules-btn");
+
+let diceValuesArr = [];
+let isModalShowing = false;
+let score = 0;
+let total = 0;
+let round = 1;
+let rolls = 0;
