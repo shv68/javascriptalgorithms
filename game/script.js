27a4a08822ce24e40e831b7d3a450ce6858321fb
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-04 18:12:35 +0530

    game-step-89

--- game/script.js
+++ game/script.js
@@ -140,6 +140,8 @@ const animate = () => {
 
     const platformDetectionRules = [
       player.position.x >= platform.position.x - player.width / 2,
+      player.position.x <=
+        platform.position.x + platform.width - player.width / 3,
     ];
   });
 };
