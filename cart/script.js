a2a20198f9d096cf50f051a5b9d1b7ea7d9db716
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-04 15:51:38 +0530

    cart-step-12

--- cart/script.js
+++ cart/script.js
@@ -89,7 +89,8 @@ products.forEach(({ name, id, price, category }) => {
   dessertCards.innerHTML += `
   <div class="dessert-card">
         <h2>${name}</h2>
-
+        <p class="dessert-price">$${price}</p>
+        <p class="product-category">Category: ${category}</p>
       </div>
   `;
 });
