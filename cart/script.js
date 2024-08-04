7eb0582a304ca3080e3e249feaa296022566f4ca
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-04 16:13:24 +0530

    cart-step-57

--- cart/script.js
+++ cart/script.js
@@ -143,9 +143,15 @@ class ShoppingCart {
       alert("Your shopping cart is already empty");
       return;
     }
+
     const isCartCleared = confirm(
       "Are you sure you want to clear all items from your shopping cart?"
     );
+
+    if (isCartCleared) {
+      this.items = [];
+      this.total = 0;
+    }
   }
 
   calculateTaxes(amount) {
