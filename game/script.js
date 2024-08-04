19bb96d94b4a9c92c9b5e10c72613544d1cf6cd1
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-04 18:04:16 +0530

    game-step-77

--- game/script.js
+++ game/script.js
@@ -111,6 +111,9 @@ const animate = () => {
     player.velocity.x = -5;
   } else {
     player.velocity.x = 0;
+
+    if (keys.rightKey.pressed && isCheckpointCollisionDetectionActive) {
+    }
   }
 };
 
