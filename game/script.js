0498d525a18f88866dc9a12fad9786f57609e8f8
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-04 18:05:27 +0530

    game-step-79

--- game/script.js
+++ game/script.js
@@ -116,6 +116,10 @@ const animate = () => {
       platforms.forEach((platform) => {
         platform.position.x -= 5;
       });
+    } else if (keys.leftKey.pressed && isCheckpointCollisionDetectionActive) {
+      platforms.forEach((platform) => {
+        platform.position.x += 5;
+      });
     }
   }
 };
