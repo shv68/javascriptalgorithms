4db824e0196f6a45173432fc067929805dceb9b5
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-04 18:41:44 +0530

    game-step-113

--- game/script.js
+++ game/script.js
@@ -208,6 +208,9 @@ const animate = () => {
         checkpoint.position.x - checkpoint.width + player.width * 0.9,
       index === 0 || checkpoints[index - 1].claimed === true,
     ];
+
+    if (checkpointDetectionRules.every((rule) => rule)) {
+    }
   });
 };
 
