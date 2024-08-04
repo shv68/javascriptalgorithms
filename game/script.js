7b2017b9337494edb5500e4adf3f81e1a76b4a43
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-04 16:31:04 +0530

    game-step-13

--- game/script.js
+++ game/script.js
@@ -13,4 +13,6 @@ let isCheckpointCollisionDetectionActive = true;
 const proportionalSize = (size) => {
   return innerHeight < 500 ? Math.ceil((size / 500) * innerHeight) : size;
 };
-class Player {}
+class Player {
+  constructor() {}
+}
