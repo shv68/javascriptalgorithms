d0b277bf3b1c05e4e80a6eff3321a23636c64000
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-04 17:47:35 +0530

    game-step-55

--- game/script.js
+++ game/script.js
@@ -96,6 +96,8 @@ const movePlayer = (key, xVelocity, isPressed) => {
       if (xVelocity === 0) {
         player.velocity.x = xVelocity;
       }
+      player.velocity.x -= xVelocity;
+      break;
   }
 };
 
