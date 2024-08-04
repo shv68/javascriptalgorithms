850c87dbb589b0f6ae20e6b8022078c650918d81
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-04 17:59:11 +0530

    game-step-69

--- game/script.js
+++ game/script.js
@@ -65,7 +65,10 @@ class Platform {
     this.width = 200;
     this.height = proportionalSize(40);
   }
-  draw() {}
+  draw() {
+    ctx.fillStyle = "#acd157";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
 }
 
 const player = new Player();
