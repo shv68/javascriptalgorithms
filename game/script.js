ffdbccec33b7af49509572ee7229ba5d66bec55f
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-04 18:31:09 +0530

    game-step-103

--- game/script.js
+++ game/script.js
@@ -153,6 +153,10 @@ const animate = () => {
       platforms.forEach((platform) => {
         platform.position.x -= 5;
       });
+
+      checkpoints.forEach((checkpoint) => {
+        checkpoint.position.x -= 5;
+      });
     } else if (keys.leftKey.pressed && isCheckpointCollisionDetectionActive) {
       platforms.forEach((platform) => {
         platform.position.x += 5;
