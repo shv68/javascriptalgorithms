2d81b0aa3a312bebb8fa5b7b181c39401789688f
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-04 17:59:59 +0530

    game-step-70

--- game/script.js
+++ game/script.js
@@ -72,7 +72,7 @@ class Platform {
 }
 
 const player = new Player();
-
+const platformPositions = [];
 const animate = () => {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
