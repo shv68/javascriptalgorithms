8dbd4efb5c873f19c2a979f901e8787832ca14dc
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-04 16:25:59 +0530

    game-step-4

--- game/script.js
+++ game/script.js
@@ -3,3 +3,4 @@ const canvas = document.getElementById("canvas");
 const startScreen = document.querySelector(".start-screen");
 const checkpointScreen = document.querySelector(".checkpoint-screen");
 const checkpointMessage = document.querySelector(".checkpoint-screen > p");
+const ctx = canvas.getContext("2d");
