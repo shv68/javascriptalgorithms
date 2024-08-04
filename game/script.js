b92e7f01f6f70f2be3ea4b682b9c7b63ed16eb17
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-04 18:23:30 +0530

    game-step-101

--- game/script.js
+++ game/script.js
@@ -121,6 +121,10 @@ const checkpointPositions = [
   { x: 4800, y: proportionalSize(80), z: 3 },
 ];
 
+const checkpoints = checkpointPositions.map(
+  (checkpoint) => new CheckPoint(checkpoint.x, checkpoint.y, checkpoint.z)
+);
+
 const animate = () => {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
