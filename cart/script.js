ed1d129142cb37fd662fce37e4fef0bfd2f11d87
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-04 16:11:38 +0530

    cart-step-53

--- cart/script.js
+++ cart/script.js
@@ -160,6 +160,7 @@ const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
   btn.addEventListener("click", (event) => {
     cart.addItem(Number(event.target.id), products);
     totalNumberOfItems.textContent = cart.getCounts();
+    cart.calculateTotal();
   });
 });
 
