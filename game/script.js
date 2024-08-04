3a7933c6aab1ebf029117516cf9ca1d813478a15
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-04 17:46:59 +0530

    game-step-54

--- game/script.js
+++ game/script.js
@@ -92,6 +92,10 @@ const movePlayer = (key, xVelocity, isPressed) => {
   }
   switch (key) {
     case "ArrowLeft":
+      keys.leftKey.pressed = isPressed;
+      if (xVelocity === 0) {
+        player.velocity.x = xVelocity;
+      }
   }
 };
 
