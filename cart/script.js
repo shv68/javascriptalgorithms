7b882350702de450294c8b5b0a93bffe9aa89a7c
Harsha Vardan <hk.harshapshv@gmail.com>
2024-08-04 15:55:27 +0530

    cart-step-21

--- cart/script.js
+++ cart/script.js
@@ -109,5 +109,7 @@ class ShoppingCart {
     const product = products.find((item) => item.id === id);
     const { name, price } = product;
     this.items.push(product);
+
+    const totalCountPerProduct = {};
   }
 }
